14bd9b64f779367c05561ca30797386ccfb4de10
Shreyans <22bcs231@iiitdmj.ac.in>
2024-05-06 05:07:29 +0530

    updated payments

--- backend/index.js
+++ backend/index.js
@@ -1,71 +1,57 @@
-const express=require('express');
-const cors=require('cors');
-const mongoose=require('mongoose');
-const Product = require('./models/product.model'); 
-let Wishlist=require('./models/wishlist.model');
-
-
-require('dotenv').config();
-const app=express();
-const port=process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: false }));
-
-const uri=process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true });
-const connection=mongoose.connection;
-connection.once('open',()=>{
-    console.log("MongoDB database connection established successfully");
-});
-
-
-
-
-
-
-const messageRouter=require('./routes/messages');
-const usersRouter=require('./routes/users');
-const wishlistRouter=require('./routes/wishlist');
-const productRouter=require('./routes/product');
-
-
-app.post("/showWishlist", async (req, res) => {
-    try {
-        const userId = req.body.userid;
-        const wishlistItems = await Wishlist.find({ userid: userId });
-
-        if (wishlistItems.length > 0) {
-            const productIds = wishlistItems.map(item => item.id);
-            const products = await Product.find({ _id: { $in: productIds } });
-            return res.json(products);
-        } else {
-            return res.json([]);
-        }
-    } catch (error) {
-        console.error(error);
-        return res.json({ message: "Error getting wishlist items. Try later", status: false });
-    }
-});
-
-
-
-
-
-app.use('/messages',messageRouter);
-app.use('/users',usersRouter);
-app.use('/wishlist',wishlistRouter);
-app.use('/product',productRouter);
-
-
-
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const messageRoutes = require("./routes/messages");
+const usersRoutes = require("./routes/users");
+const wishlistRoutes = require("./routes/wishlist");
+const productRoutes = require("./routes/product");
+const Product = require("./models/product.model");
+let Wishlist = require("./models/wishlist.model");
 const Razorpay = require("razorpay");
-
 const crypto = require("crypto");
 
 
+const port = process.env.PORT || 3000;
+const uri = process.env.ATLAS_URI;
+const corsOptions = {
+  origin: "http://localhost:5173", // to allow requests from client at port 5173
+  credentials: true, // to allow cookies from client
+};
 
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+
+mongoose.connect(uri, { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB database connection established successfully");
+  app.listen(port, () => {
+    console.log(`Server is running on port:${port}`);
+  });
+});
+
+app.post("/showWishlist", async (req, res) => {
+  try {
+    const userId = req.body.userid;
+    const wishlistItems = await Wishlist.find({ userid: userId });
+
+    if (wishlistItems.length > 0) {
+      const productIds = wishlistItems.map((item) => item.id);
+      const products = await Product.find({ _id: { $in: productIds } });
+      return res.json(products);
+    } else {
+      return res.json([]);
+    }
+  } catch (error) {
+    console.error(error);
+    return res.json({
+      message: "Error getting wishlist items. Try later",
+      status: false,
+    });
+  }
+});
 
 app.post("/order", async (req, res) => {
   try {
@@ -87,7 +73,6 @@ app.post("/order", async (req, res) => {
     res.status(500).send("Error");
   }
 });
-
 app.post("/order/validate", async (req, res) => {
   const { razorpay_order_id, razorpay_payment_id, razorpay_signature } =
     req.body;
@@ -107,6 +92,10 @@ app.post("/order/validate", async (req, res) => {
   });
 });
 
-app.listen(port, () => {
-  console.log("Listening on port", port);
-});
+
+app.use("/messages", messageRoutes);
+app.use("/users", usersRoutes);
+app.use("/wishlist", wishlistRoutes);
+app.use("/products", productRoutes);
+
+
