96a7dd93841f103f7c8a5fec505664c56b2fbb0f
Akash Paloju <akashpaloju@gmail.com>
2024-05-06 01:02:20 +0530

    Fix wishlist page

--- backend/models/wishlist.model.js
+++ backend/models/wishlist.model.js
@@ -2,26 +2,26 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const wishlistSchema = new Schema(
-	{
-		userid: { type: String, required: true },
-		wishes: { type: Array, required: true },
-	},
-	{
-		timestamps: true,
-	}
+  {
+    id: { type: String, default: null },
+    userid: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
 );
 
 const reviewSchema = new Schema(
-	{
-		id: { type: String, required: true },
-		product_id: { type: String, required: true },
-		userid: { type: String, required: true },
-		content: { type: String, required: true },
-		rating: { type: Number, required: true },
-	},
-	{
-		timestamps: true,
-	}
+  { 
+    id: { type: String, required: true },
+    product_id: { type: String, required: true },
+    userid: { type: String, required: true },
+    content : { type: String, required: true },
+    rating : { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  },
 );
 const Wishlist = mongoose.model("wishlist", wishlistSchema);
 module.exports = Wishlist;
